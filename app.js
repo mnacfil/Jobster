bd9e0fe3bf2aa1d0e9f40983f0607b61f852f4d9
Melvin Nacfil <mnacfil@gmail.com>
2022-09-25 01:01:03 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express')
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+})
